229fde15f84066c9b02dfc531a61dbb8c76b0fa8
Blake Frederick <bf@six12creative.com>
2024-10-28 21:28:21 -0700

    sign in + sign out

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import Image from "next/image"
+import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+        <Link href="/" className="flex items-center space-x-2">
+            <div className="bg-green-500 w-fit">
+                <Image src="/filament-cross.png" alt="Filament" width={70} height={70} />
+            </div>
+            <h1 className="font-bold text-xl">Filament</h1>
+        </Link>
+
+        <div className="px-5 flex space-x-2 items-center">
+            <UserButton afterSignOutUrl="/" />
+            <SignedOut>
+                <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+            </SignedOut>
+        </div>
+    </header>
+  )
+}
+
+export default Header
